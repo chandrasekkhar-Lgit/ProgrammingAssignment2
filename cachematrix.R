f34e589bcca2c24f21de4f363403d8f87dab915b
chandrasekkhar-Lgit <65726667+chandrasekkhar-Lgit@users.noreply.github.com>
2020-05-22 22:55:27 +0530

    getting ready to push an example cachematrixfile

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Below functions are written to eiliminate repetitive calculations of matrix inversions
+##when input matix data is variable which might have similar elemenst as in past.
+##Same would be obtained by creating two functions- makeCacheMatrix and CacheSolve.
 
-## Write a short comment describing this function
+##makeCacheMatrix() creates a matrix in form of R object and stores the matrix 
+##and the inverse of matix.Returns list of functions as set(), get(),setinv() 
+##and getinv().
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<- NULL    # inv represents Inverse of Matrix.Here assigning NULL value.
+    
+    # set function inputs argument to x in the Parent environment and assigns
+    #NULL value to the inv object.
+    set<-function(y){ 
+          x<<-y
+          inv<<-NULL
+    }   
+    
+    get<-function() x   # X is called as argument from parent environment.  
+    
+    set_inv<-function(solvex) inv<<-solvex #set_inv function is created.    
+             #input argument is assigned to inv in parent environment.
+    
+    get_inv<-function() inv  # getter function for the elements of inv. 
+    
+    list(set=set, get=get, set_inv=set_inv, get_inv=get_inv) 
+    # returns makeCacheMatrix() object, with above mentioned functions
+    #as the elements of list.
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function calls the argument that is returned from makeCacheMatrix().
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(z, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## cacheSolve() would be populating the inverse of matrix from object of makeCacheMatrix()
+    
+    inv<-z$get_inv() # calls get_inv function on the input object
+  
+    # For a new matrix since inv is set to NULL in makeCacheMatrix(), 
+    #if the argument is not NULL, we have valid Cached inv values and would return it.  
+        if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    } 
+    
+    #if !is.null() is false,then below is execution of Inverse of Matrix function   
+    #by fetching data from input matrix, calculates inverse of Matrix. Uses set_inv
+    #to set the inverse of matrix in the input object i.e.makeCacheMatrix. Then returns inv value
+    
+    data<-z$get()
+    inv<- solve(data,...)
+    z$set_inv(inv)
+    inv
 }
